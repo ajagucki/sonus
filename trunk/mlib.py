e09a0107c8b7a2dbef53dc1e4f909bd5a2eedacc
ajagucki <ajagucki@b9f4b7c3-fd24-0410-8f6a-4f7397c08025>
2006-12-21 23:08:49 +0000

    Cleaned up mlib.py to better conform to 'Style Guide for Python Code' PEP 8

--- trunk/mlib.py
+++ trunk/mlib.py
@@ -8,15 +8,18 @@ class Mlib:
         self.sonus = sonus
 
     def search(self, search_type, search_string, ret):
-        query = "select distinct m1.id as id from Media m1 left join Media m2 on m1.id = m2.id and m2.key = 'resolved' and m2.value = 1"
+        query  = "SELECT DISTINCT m1.id AS id FROM Media m1 LEFT JOIN "
+        query += "Media m2 ON m1.id = m2.id AND m2.key = 'resolved' AND "
+        query += "m2.value = 1 "
 
         if search_type == "all":
-            query += " where lower(m1.value) like '%%%s%%'" % (search_string)
+            query += "WHERE lower(m1.value) LIKE '%%%s%%' " % (search_string)
         else:
-            query += " where m1.key = '%s' and lower(m1.value) like '%%%s%%'" % (search_type, search_string)
+            query += "WHERE m1.key = '%s' AND lower(m1.value) LIKE '%%%s%%' " \
+                   % (search_type, search_string)
 
-        query += " order by m1.id"
-        
+        query += "ORDER BY m1.id"
+
         handler = TrackListHandler(ret)
         self.sonus.medialib_select(query, handler.callback)
 
@@ -31,19 +34,19 @@ class Mlib:
 class TrackListHandler:
     def __init__(self, ret):
         self.ret = ret
-    
+
     def callback(self, track_list):
         track_list = track_list.value()
         track_list = [track["id"] for track in track_list]
-        
+
         self.ret(track_list)
 
 class TrackInfoHandler:
     def __init__(self, ret, *args):
         self.ret = ret
         self.args = args
-    
+
     def callback(self, track_info):
         track_info = track_info.value()
-        
+
         self.ret(track_info, *self.args)
