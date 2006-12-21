a4710ba5d79893efd6878999fa472d5b3d4fb118
ajagucki <ajagucki@b9f4b7c3-fd24-0410-8f6a-4f7397c08025>
2006-12-21 14:01:18 +0000

    Merged the QApplication object with gui.MainWindow, so our main loop is executed in MainWindow.

--- gui.py
+++ gui.py
@@ -6,21 +6,36 @@ For use with Sonus, a PyQt4 XMMS2 client.
 from PyQt4 import QtGui
 import xmmsqt4
 
-class Gui(QtGui.QApplication):
+class MainWindow(QtGui.QMainWindow):
     def __init__(self, sonus, argv):
-        QtGui.QApplication.__init__(self, argv)
+        self.app = QtGui.QApplication(argv)
+        self.app.setApplicationName('Sonus')
+
+        QtGui.QMainWindow.__init__(self)
+        self.setWindowTitle('Sonus')
+
+        # Create our widgets
+        self.createStatusBar()
 
         # Connect our event loop with Sonus
         if sonus.is_connected():
-            xmmsqt4.XMMSConnector(self, sonus)
+            xmmsqt4.XMMSConnector(self.app, sonus)
+            self.statusBar().showMessage(self.tr('Connected to xmms2d'))
+        else:
+            self.statusBar().showMessage(self.tr('Not connected to xmms2d'))
+
+    def createStatusBar(self):
+        self.statusBar().showMessage(self.tr('Ready'))
 
     def run(self):
         """
-        Show the player interface and begin the event loop
+        Show the main window and begin the event loop
         """
-        # mainWidget = MainWidget()
-        # mainWidget.show()
-        self.exec_()
+        self.show()
+        return self.app.exec_()
 
     def handle_disconnect(self):
-        self.quit()
+        """
+        Handle a disconnection between Sonus and xmms2d
+        """
+        self.app.quit()
