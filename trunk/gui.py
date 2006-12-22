fad014f465c93f095e4624e758eb448db211d080
ajagucki <ajagucki@b9f4b7c3-fd24-0410-8f6a-4f7397c08025>
2006-12-22 13:55:55 +0000

    Created gui.MlibDialog, which offers access to mlib.Mlib functionality.

--- trunk/gui.py
+++ trunk/gui.py
@@ -15,6 +15,9 @@ class MainWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         self.setWindowTitle('Sonus')
 
+        # Encapsulate our dialogs
+        self.mlib_dialog = MlibDialog(self)
+
         # Create our widgets
         self.create_status_bar()
         self.create_test_button()
@@ -30,9 +33,9 @@ class MainWindow(QtGui.QMainWindow):
         self.statusBar().showMessage(self.tr('Ready'))
 
     def create_test_button(self):
-        self.test_button = QtGui.QPushButton(self.tr('Track Info'), self)
+        self.test_button = QtGui.QPushButton(self.tr('Media Library'), self)
         self.connect(self.test_button, QtCore.SIGNAL('clicked()'),
-                     self.test_button_wrapper)
+                     self.mlib_dialog.show)
 
     def test_button_wrapper(self):
         self.sonus.mlib.get_track_info(1, self.test_button_callback)
@@ -66,3 +69,77 @@ class MainWindow(QtGui.QMainWindow):
         err_msg.showMessage(msg)
         err_msg.exec_()
         self.app.quit()
+
+
+class MlibDialog(QtGui.QDialog):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+
+        self.grid_layout = QtGui.QGridLayout(self)
+
+        self.frame = QtGui.QFrame(self)
+
+        self.grid_layout1 = QtGui.QGridLayout(self.frame)
+
+        self.vbox_layout = QtGui.QVBoxLayout()
+
+        self.hbox_layout = QtGui.QHBoxLayout()
+
+        self.label = QtGui.QLabel(self.frame)
+        self.hbox_layout.addWidget(self.label)
+
+        self.search_line_edit = QtGui.QLineEdit(self.frame)
+        self.hbox_layout.addWidget(self.search_line_edit)
+        self.vbox_layout.addLayout(self.hbox_layout)
+
+        self.list_view = QtGui.QListView(self.frame)
+        self.vbox_layout.addWidget(self.list_view)
+        self.grid_layout1.addLayout(self.vbox_layout, 0, 0)
+        self.grid_layout.addWidget(self.frame)
+
+        self.hbox_layout1 = QtGui.QHBoxLayout()
+
+        spacer_item = QtGui.QSpacerItem(131, 31, QtGui.QSizePolicy.Expanding,
+                                        QtGui.QSizePolicy.Minimum)
+        self.hbox_layout1.addItem(spacer_item)
+
+        self.add_button = QtGui.QPushButton(self)
+        self.add_button.setObjectName("add_button")
+        self.hbox_layout1.addWidget(self.add_button)
+
+        self.remove_button = QtGui.QPushButton(self)
+        self.remove_button.setObjectName("remove_button")
+        self.hbox_layout1.addWidget(self.remove_button)
+        self.grid_layout.addLayout(self.hbox_layout1, 1, 0)
+        self.label.setBuddy(self.search_line_edit)
+
+        self.setWindowTitle(self.tr("Media Library"))
+        self.label.setText(self.tr("&Search:"))
+        self.add_button.setText(self.tr("&Add"))
+        self.remove_button.setText(self.tr("&Remove"))
+
+        self.connect(self.add_button, QtCore.SIGNAL("clicked()"),
+            self.add_media)
+        self.connect(self.remove_button, QtCore.SIGNAL("clicked()"),
+            self.remove_media)
+
+        self.setTabOrder(self.search_line_edit,self.list_view)
+        self.setTabOrder(self.list_view,self.add_button)
+        self.setTabOrder(self.add_button,self.remove_button)
+
+    def add_media(self):
+        """
+        Add media to the XMMS2 media library.
+        """
+        audio_files = QtGui.QFileDialog.getOpenFileNames(
+                        self,
+                        "Add Audio Files",
+                        "/home",
+                        "Audio (*.mp3 *.ogg *.flac)")
+        # TODO: Attempt to add selected files to mlib
+
+    def remove_media(self):
+        """
+        Remove media from the XMMS2 media library.
+        """
+        None # Not implemented
