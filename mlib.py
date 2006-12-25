f56d5b817370bf40c9d987c8be961d587dcdca7d
bslote <bslote@b9f4b7c3-fd24-0410-8f6a-4f7397c08025>
2006-12-25 09:38:55 +0000

    Better logging

--- mlib.py
+++ mlib.py
@@ -6,10 +6,13 @@ For use with Sonus, a PyQt4 XMMS2 client.
 from PyQt4 import QtCore
 from xmmsclient import Universe
 
+import logging
+
 
 class Mlib(QtCore.QObject):
     def __init__(self, sonus, parent=None):
         self.sonus = sonus
+        self.logger = logging.getLogger('sonusLogger.mlib.Mlib')
         QtCore.QObject.__init__(self, parent)
         self.idList = []
 
@@ -18,14 +21,14 @@ class Mlib(QtCore.QObject):
         Order up a list of all the tracks in the media library
         """
         allmedia = Universe()
-        print "From getAllMedia():", allmedia   #DEBUG
+        self.logger.debug("From getAllMedia(): %s" % allmedia)   #DEBUG
         self.sonus.coll_query_ids(allmedia, cb=self.callback)
 
     def callback(self, result):
         """
         Callback for the collection query
         """
-        print "From callback():", result    #DEBUG
+        self.logger.debug("From callback(): %s" % result)    #DEBUG
         if not result.iserror():
             self.idList = result.value()
         else:
@@ -34,5 +37,5 @@ class Mlib(QtCore.QObject):
         """
         Now we want to try and emit a signal to inform the gui
         """
-        print "mlibmodel: Emitting singal 'got_all_media()'"    #DEBUG
+        self.logger.debug("mlibmodel: Emitting singal 'got_all_media()'")    #DEBUG
         self.emit(QtCore.SIGNAL('got_all_media()'))
